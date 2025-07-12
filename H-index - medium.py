6ea42c66ea678d34aa38cdf231dd4845d62dec3d
Rashi Tyagi <56127515+ratyagi@users.noreply.github.com>
2025-07-12 18:38:13 -0500

    fixed bug

--- H-index - medium.py
+++ H-index - medium.py
@@ -1,4 +1,5 @@
 # Approach: first sort then find first index more than or equal to its value. that is h index
+# [6,5,3,1,0] --> [6,5,3,1,0]. if val of h<sorted_arr[h] then h++. return h
 # Time: O(nlogn)
 # Space: O(1)
 
@@ -9,7 +10,7 @@ class Solution:
 
         while h < len(sorted_arr) and h < sorted_arr[h]:
             h+=1
-        return 
+        return h
 
 # Test case
 print(Solution().hIndex([3, 3, 3]))  # Expected Output: 3
