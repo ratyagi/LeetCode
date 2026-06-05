c76a487d33ba7625b9291d503aea44fcc3d83cfe
Rashi Tyagi <56127515+ratyagi@users.noreply.github.com>
2026-06-04 20:16:11 -0500

    Implement productExceptSelf method in Solution class

---
+++ product except self -med.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1]* len(nums)
+        prefix, postfix = 1, 1
+
+        #prefix
+        for i in range(len(nums)):
+            res[i]=prefix
+            prefix*= nums[i]
+        #postfix
+        for i in range(len(nums)-1, -1, -1):
+            res[i]*=postfix
+            postfix*= nums[i]
+        return res
+        
